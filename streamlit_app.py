0b62e622c95e5272bd919ac6c2d80faf7032f1cd
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-03 17:15:17 -0500

    Update streamlit_app.py
    add 'inplace' attribute

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 import pandas
 my_fruits_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruits_list.set_index('Fruit')
+my_fruits_list.set_index('Fruit', inplace=True)
 streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
 streamlit.dataframe(my_fruits_list)
 
