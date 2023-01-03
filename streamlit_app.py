2583f87f0f7ec1fd6d182aba0d52221e9c9edb81
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-03 17:28:36 -0500

    Update streamlit_app.py
    Only show the selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 import pandas
 my_fruits_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruits_list.set_index('Fruit', inplace=True)
-streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index), ['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruits_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
 
