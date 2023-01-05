79471ef0db1c3f665d77b49ff6f7fb2ba038bf10
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-05 18:15:40 -0500

    Update streamlit_app.py
    display json part in response

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list
 fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-
+#new section to display  fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json)
