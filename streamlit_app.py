7b2e23ee16e3c07bc5dd18179be581bfc3033100
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-05 18:13:09 -0500

    Update streamlit_app.py
    add a variable for response and display it

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,6 @@ fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
