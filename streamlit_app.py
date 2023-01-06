c37c1ddf135903549551add1703221b5ad1ea709
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-05 23:05:11 -0500

    Update streamlit_app.py
    connected to snowflake trail account and got its metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display the table/df in webapp
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row  = my_cur.fetchone()
+streamlit.text("Hello from snowflake")
+streamlit.text(my_data_row)
