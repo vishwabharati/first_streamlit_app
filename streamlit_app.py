57f83add55f8d6549506aed98ef03dce33efe121
vishwabharati <42681475+vishwabharati@users.noreply.github.com>
2023-01-06 13:25:22 -0500

    Update streamlit_app.py
    add function to insert into table every time a  new fruit name is suggested

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,13 @@ if streamlit.button('Get Fruit load list'):
    streamlit.dataframe(my_data_rows)
    
 streamlit.stop()
-add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
-streamlit.write("Thanks for adding", add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+      return "Thanks for adding" + new_fruit
+     
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+if streamlit.button("Add a fruit to the list"):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   streamlit.text(insert_row_snowflake(add_my_fruit))
+
